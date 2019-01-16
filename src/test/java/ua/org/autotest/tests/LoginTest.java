9df669be4e4fb790cd9c137bfbbab6204294f0ac
Yarmish.Yura <yarmish.yura@gmail.com>
2019-01-16 16:39:16 +0200

    comit and push

--- src/test/java/ua/org/autotest/tests/LoginTest.java
+++ src/test/java/ua/org/autotest/tests/LoginTest.java
@@ -12,22 +12,7 @@ import ua.org.autotest.pages.MailBoxPage;
 
 import java.util.concurrent.TimeUnit;
 
-public class LoginTest {
-    public static WebDriver driver;
-    public static LoginPage loginPage;
-    public static MailBoxPage mailBoxPage;
-
-    @BeforeClass
-    public static void setUp(){
-        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Java\\chromedriver.exe");
-        driver = new ChromeDriver();
-        loginPage = new LoginPage(driver);
-        mailBoxPage = new MailBoxPage(driver);
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://accounts.google.com/ServiceLogin/identifier?service=mail&passive=true&rm=false&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1&flowName=GlifWebSignIn&flowEntry=AddSession");
-    }
-
+public class LoginTest extends SetUpTest {
     @Test
     public void loginTest(){
         loginPage.inputLogin("yarmish.yura12345@gmail.com");
@@ -38,11 +23,5 @@ public class LoginTest {
         mailBoxPage.entryMenu();
         String mailUser = mailBoxPage.getUserMail();
         Assert.assertEquals("yarmish.yura12345@gmail.com", mailUser);
-    }
-
-    @AfterClass
-    public static void tearDown(){
-        mailBoxPage.userLogout();
-        driver.quit();
     }
 }
---
+++ src/test/java/ua/org/autotest/tests/SetUpTest.java
@@ -0,0 +1,33 @@
+package ua.org.autotest.tests;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import ua.org.autotest.pages.LoginPage;
+import ua.org.autotest.pages.MailBoxPage;
+
+import java.util.concurrent.TimeUnit;
+
+public class SetUpTest{
+    public static WebDriver driver;
+    public static LoginPage loginPage;
+    public static MailBoxPage mailBoxPage;
+
+    @BeforeClass
+    public static void setUp(){
+        System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
+        driver = new ChromeDriver();
+        loginPage = new LoginPage(driver);
+        mailBoxPage = new MailBoxPage(driver);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://accounts.google.com/ServiceLogin/identifier?service=mail&passive=true&rm=false&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1&flowName=GlifWebSignIn&flowEntry=AddSession");
+    }
+
+    @AfterClass
+    public static void tearDown(){
+        mailBoxPage.userLogout();
+        driver.quit();
+    }
+}
